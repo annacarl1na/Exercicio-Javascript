b135b552488ad88661f037c9736ea70db032cb3f
Anna Carolina <103374801+annacarl1na@users.noreply.github.com>
2022-08-14 21:51:33 -0300

    Add files via upload

---
+++ exerc26.js
@@ -0,0 +1,7 @@
+/*************************************************************************
+26) Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+*************************************************************************/
+//<h3>Exercício 26</h3>
+for(i=100; i>=0; i--){
+    document.write(i +"<br>");
+}   
